d4abeb693baa43a92454e9186a8c4224eeb0bc9b
AnudeepCHS <60534419+AnudeepCHS@users.noreply.github.com>
2020-11-22 15:35:00 -0800

    git ignore config

--- backend/config/configuration.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  secret: 'CMPE273YelpLab2jwtsecret',
-  mongoDB:'mongodb+srv://AnudeepSJSU:cmpe_273@cluster0.q5b4r.mongodb.net/273_lab2?retryWrites=true&w=majority',
-};
-
-module.exports = config;
