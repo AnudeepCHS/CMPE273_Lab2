587c3091a56bc6a49cbda080ff49d29c0a3edb47
AnudeepCHS <60534419+AnudeepCHS@users.noreply.github.com>
2020-11-23 08:47:38 -0800

    Update configuration.js

--- kafka/config/configuration.js
+++ kafka/config/configuration.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoDB:'mongodb+srv://AnudeepSJSU:cmpe_273@cluster0.q5b4r.mongodb.net/273_lab2?retryWrites=true&w=majority',
+  mongoDB:'mongodb+srv://AnudeepSJSU:cmpe_273@cluster0.q5b4r.mongodb.net/273_Lab2?retryWrites=true&w=majority',
 };
 
 module.exports = config;
